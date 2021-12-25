3d52ba7b494c9d9eb9cacb142ad4a94bfcaad844
Dmitry Uvarov <dm.a.uvarov@gmail.com>
2021-12-25 17:36:58 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }// end if
+}// end functon moveDodgerLeft
+
+
+document.addEventListener("keydown", function (e) {
+    console.log(e);
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+      } 
+});
+
+// const 
+
+
+
+function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+    
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    
+    }// end if
+}
+
+
